883a9713fb060d36116d9b5745b6cf7050fbef9d
valovski <vladi2005123@gmail.com>
2022-07-06 22:59:11 +0300

    Fix errors, create TileMap drawing function and more

--- scripts/TileMap.js
+++ scripts/TileMap.js
@@ -1,6 +1,12 @@
 export default class TileMap {
   constructor(tileSize) {
     this.tileSize = tileSize;
+
+    this.yellowDot = new Image();
+    this.yellowDot.src = "../images/yellowDot.png";
+
+    this.wall = new Image();
+    this.wall.src = "../images/wall.png";
   }
 
   map = [
@@ -17,5 +23,29 @@ export default class TileMap {
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   ];
 
-  draw() {}
+  draw(ctx) {
+    for (let row = 0; row < this.map.length; row++) {
+      for (let column = 0; column < this.map[row].length; column++) {
+        let tile = this.map[row][column];
+        if (tile === 1) {
+          this.#drawWall(ctx, column, row, this.tileSize);
+        }
+      }
+    }
+  }
+
+  #drawWall(ctx, column, row, size) {
+    ctx.drawImage(
+      this.wall,
+      column * this.tileSize,
+      row * this.tileSize,
+      size,
+      size
+    );
+  }
+
+  setCanvasSize(canvas) {
+    canvas.width = this.map[0].length * this.tileSize;
+    canvas.height = this.map.length * this.tileSize;
+  }
 }
