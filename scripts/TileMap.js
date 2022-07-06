f48c890c7311a17c1431afb29bf620b753680896
valovski <vladi2005123@gmail.com>
2022-07-06 23:22:43 +0300

    Add yellow dot drawing function to TileMap.js

--- scripts/TileMap.js
+++ scripts/TileMap.js
@@ -10,12 +10,12 @@ export default class TileMap {
   }
 
   map = [
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -29,9 +29,21 @@ export default class TileMap {
         let tile = this.map[row][column];
         if (tile === 1) {
           this.#drawWall(ctx, column, row, this.tileSize);
+        } else if (tile === 0) {
+          this.#drawDot(ctx, column, row, this.tileSize);
         }
       }
     }
+  }
+
+  #drawDot(ctx, column, row, size) {
+    ctx.drawImage(
+      this.yellowDot,
+      column * this.tileSize,
+      row * this.tileSize,
+      size,
+      size
+    );
   }
 
   #drawWall(ctx, column, row, size) {
