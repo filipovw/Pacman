906fa6e7d48eefb0bd8114eb03c481d1797d9e27
valovski <vladi2005123@gmail.com>
2022-07-07 00:23:18 +0300

    Implement movement of character up and down

--- scripts/Pacman.js
+++ scripts/Pacman.js
@@ -7,10 +7,17 @@ export default class Pacman {
     this.tileSize = tileSize;
     this.velocity = velocity;
     this.tileMap = tileMap;
+
+    this.currentMovingDirection = null;
+    this.requestedMovingDirection = null;
+
+    document.addEventListener("keydown", this.#keydown);
+
     this.#loadPacmanImages();
   }
 
   draw(ctx) {
+    this.#move();
     ctx.drawImage(
       this.pacmanImages[this.pacmanImageIndex],
       this.x,
@@ -41,5 +48,55 @@ export default class Pacman {
     ];
 
     this.pacmanImageIndex = 0;
+  }
+
+  #keydown = (event) => {
+    //up
+    if (event.keyCode == 38) {
+      if (this.currentMovingDirection == MovingDirection.down) {
+        this.currentMovingDirection = MovingDirection.up;
+      }
+      this.requestedMovingDirection = MovingDirection.up;
+    }
+    //down
+    if (event.keyCode == 40) {
+      if (this.currentMovingDirection == MovingDirection.up) {
+        this.currentMovingDirection = MovingDirection.down;
+      }
+      this.requestedMovingDirection = MovingDirection.down;
+    }
+    //left
+    if (event.keyCode == 37) {
+      if (this.currentMovingDirection == MovingDirection.right) {
+        this.currentMovingDirection = MovingDirection.left;
+      }
+      this.requestedMovingDirection = MovingDirection.left;
+    }
+    //right
+    if (event.keyCode == 39) {
+      if (this.currentMovingDirection == MovingDirection.left) {
+        this.currentMovingDirection = MovingDirection.right;
+      }
+      this.requestedMovingDirection = MovingDirection.right;
+    }
+  };
+
+  #move() {
+    if (this.currentMovingDirection !== this.requestedMovingDirection) {
+      if (
+        Number.isInteger(this.x / this.tileSize) &&
+        Number.isInteger(this.y / this.tileSize)
+      ) {
+        this.currentMovingDirection = this.requestedMovingDirection;
+      }
+    }
+    switch (this.currentMovingDirection) {
+      case MovingDirection.up:
+        this.y -= this.velocity;
+        break;
+      case MovingDirection.down:
+        this.y += this.velocity;
+        break;
+    }
   }
 }
